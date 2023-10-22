efb2d7764d859bd2c8fd52f3e94e07fc1cb4202c
KazeKagi <safrizalrahman46@gmail.com>
2023-10-22 22:57:34 +0700

    Push 2 + modify is a leap year To Github SAFRIZAL RAHMAN

--- Selection2Exp222.java
+++ Selection2Exp222.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Selection2Exp222 {
     public static void main(String[] args) {
         Scanner input24 = new Scanner(System.in); 
+
         int angle1, angle2, angle3, totalAngle;
         System.out.print("Input angle 1 : ");
         angle1 = input24.nextInt(); 
@@ -12,12 +13,28 @@ public class Selection2Exp222 {
         angle3 = input24.nextInt(); 
 
         totalAngle = angle1 + angle2 + angle3; 
-        if (totalAngle==180) { 
+        if (totalAngle == 180) {
             if (angle1 == 90 || angle2 == 90 || angle3 == 90) {
-                System.out.println("Right Triangle"); 
+                System.out.println("Right Triangle");
+            } else if (angle1 == angle2 && angle2 == angle3) {
+                System.out.println("Equilateral Triangle");
+            } else if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3) {
+                System.out.println("Isosceles Triangle");
             } else {
-                System.out.println("Not a Right Triangle "); } 
-            } else { 
-                System.out.println("Not a Trinagle"); }
+                System.out.println("Scalene Triangle");
+            }
+        } else {
+            System.out.println("Not a Triangle");
+        }
+
+        // totalAngle = angle1 + angle2 + angle3; 
+        // if (totalAngle==180) { 
+        //     if (angle1 == 90 || angle2 == 90 || angle3 == 90) {
+        //         System.out.println("Right Triangle"); 
+        //     } else {
+        //         System.out.println("Not a Right Triangle "); } 
+        //     } else { 
+        //         System.out.println("Not a Trinagle"); 
+        //     }
     }
 }
