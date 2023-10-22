b461e2349d4e6ce62575f9b3dba8e6bd76fd4a44
KazeKagi <safrizalrahman46@gmail.com>
2023-10-22 22:29:08 +0700

    Push 4 + Print 2100 + 2000 is a leap year To Github SAFRIZAL RAHMAN

--- Selection2Exp1Student22.java
+++ Selection2Exp1Student22.java
@@ -3,17 +3,29 @@ public class Selection2Exp1Student22 {
     public static void main(String[] args) {
         Scanner inputan22 = new Scanner(System.in); 
         int year; System.out.println("What Year ?"); 
-        
         year = inputan22.nextInt(); 
         
-        if (year%4==0) {
-            if (year%100!=0) {
-                System.out.println("Leap Year"); 
-        }   else {
-                System.out.println("Not A leap Year");
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Leap Year");
+                } else {
+                    System.out.println("Not A Leap Year");
+                }
+            } else {
+                System.out.println("Leap Year");
+            }
+        } else {
+            System.out.println("Not A Leap Year");
         }
-        }   else { 
-            System.out.println("Not A leap Yea"); }
+        // if (year%4==0) {
+        //     if (year%100!=0) {
+        //         System.out.println("Leap Year"); 
+        // }   else {
+        //         System.out.println("Not A leap Year");
+        // }
+        // }   else { 
+        //     System.out.println("Not A leap Yea"); }
     }
 }
 
